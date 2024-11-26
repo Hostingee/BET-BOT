90ee86ff530b512a1937912a7f70432d8925ad9d
Hostingee <x13631260@gmail.com>
2024-11-26 11:04:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,8 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello_world():
-
     return 'Shadow'
 
 if __name__ == "__main__":
-
-    app.run()
+    app.run(host="0.0.0.0", port=8000)  # Make sure to listen on all interfaces
